2b010dcc45eac27b53057756f39fe3103ae10674
AlvarDVK <alvarc98@gmail.com>
2021-01-25 20:41:08 +0100

    Creación de Enigma

---
+++ Enigma.py
@@ -0,0 +1,13 @@
+""" 
+Enigma machine Code.
+Each letter is associated with another random one.
+When a letter is selected, it changes so it has to be 
+pushed back again 26 times.
+We are gonna simmulate objects that work as rotators 
+and reflectors. 
+"""
+
+rotorI = 1
+
+def rotor():
+    
